7a06d75a7fb0f7b8030cf0d4e5b5aeda0b11fe79
yuezht <75134974@qq.com>
2017-10-21 16:28:34 +0800

    pointer test

--- hello-world/hello_world.go
+++ hello-world/hello_world.go
@@ -4,7 +4,23 @@ import (
 	"fmt"
 )
 
-func main()  {
+func main() {
 	fmt.Printf("Hello Golang!\n")
+	x := 1
+	p := &x
+	fmt.Printf(":%d\n", *p)
+	fmt.Printf(":%d\n", p)
+	fmt.Printf(":%d\n", x)
+	var d *int
+	d=f()
+	fmt.Printf(":%d\n", d)
+	fmt.Printf(":%d\n", f())
+	fmt.Printf(":%d\n", *d)
 }
 
+
+
+func f() *int{
+	v :=1
+	return &v
+}
