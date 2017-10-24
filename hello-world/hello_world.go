37b746007f569691d7cbd7262a42e85dcb6ebce2
yuezht <75134974@qq.com>
2017-10-24 14:55:25 +0800

    pointer test

--- hello-world/hello_world.go
+++ hello-world/hello_world.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/cmplx"
+	"strings"
+	"math"
 )
 
 func main() {
@@ -12,15 +15,25 @@ func main() {
 	fmt.Printf(":%d\n", p)
 	fmt.Printf(":%d\n", x)
 	var d *int
-	d=f()
+	d = f()
 	fmt.Printf(":%d\n", d)
 	fmt.Printf(":%d\n", f())
 	fmt.Printf(":%d\n", *d)
+
+	fmt.Println(cmplx.Sqrt(-1))
+
+	var testString string = `hsfgys\nfsfs""`
+	var runeTest rune = 'd'
+
+	fmt.Println(testString)
+	fmt.Println(runeTest)
+	fmt.Println(strings.Contains(testString,"fg"))
+	var a = math.Pi
+	fmt.Println(a)
+
 }
 
-
-
-func f() *int{
-	v :=1
+func f() *int {
+	v := 1
 	return &v
 }
